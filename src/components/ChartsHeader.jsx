46654466e79def6ca97645b9797a457e9b3bf458
marsta96 <startek.marta@gmail.com>
2023-06-06 17:28:04 +0200

    Preparing settings and structure

---
+++ src/components/ChartsHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChartsHeader = () => {
+  return (
+    <div>
+      Chat Header
+    </div>
+  )
+}
+
+export default ChartsHeader
---
+++ src/pages/Charts/Pyramid.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Pyramid = () => {
+  return (
+    <div>Pyramide</div>
+  )
+}
+
+export default Pyramid
